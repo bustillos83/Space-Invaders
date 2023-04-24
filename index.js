79999cfe36e0aec6a446d999bce7dc91f1097141
Fernando Bustillos <bustillos007@gmail.com>
2023-04-24 16:13:12 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = innerWidth;
+canvas.width = innerHeight;
+
+class Player{
+    constructor(){
+        
+    }
+}
