43533095221d9d9538fc823ffca7dd781a44f026
Fernando Bustillos <bustillos007@gmail.com>
2023-04-28 14:34:02 -0400

    adding projectiles

--- index.js
+++ index.js
@@ -57,6 +57,28 @@ class Player{
     }
 }
 
+class Projectile{
+    constructor({position, velocity}){
+        this.position = position
+        this.velocity = velocity
+        this.radius = 3
+    }
+
+
+    draw(){
+        c.beginPath()
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+        c.fillStyle = 'red'
+        c.fill()
+        c.closePath()
+    }
+    update(){
+        this,draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+
+    }
+}
 
 
 const player = new Player()
