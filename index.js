f4809804ca7c7d6138f72460452b7a55762fe3aa
Fernando Bustillos <bustillos007@gmail.com>
2023-04-30 21:26:40 -0400

    added invader

--- index.js
+++ index.js
@@ -84,8 +84,56 @@ class Projectile {
   }
 }
 
+class Invader {
+  constructor() {
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+
+    const image = new Image();
+    image.src = "./img/invader2.png";
+    image.onload = () => {
+      const scale = 1;
+      this.image = image;
+      this.width = image.width * scale;
+      this.height = image.height * scale;
+      this.position = {
+        x: canvas.width / 2 - this.width / 2,
+        y: canvas.height / 2,
+      };
+    };
+  }
+
+  draw() {
+    // c.fillStyle = 'red'
+    // c.fillRect(this.position.x,this.position.y, this.width, this.height)
+
+    // c.translate(
+    //   -player.position.x - player.width / 2,
+    //   -player.position.y - player.height / 2
+    // );
+
+    c.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+    );
+  }
+
+  update() {
+    if (this.image) {
+      this.draw();
+      this.position.x += this.velocity.x;
+      this.position.y += this.velocity.y;
+    }
+  }
+}
 const player = new Player();
 const projectiles = [];
+const invader = new Invader();
 const keys = {
   a: {
     pressed: false,
@@ -102,6 +150,7 @@ function animate() {
   requestAnimationFrame(animate);
   c.fillStyle = "black";
   c.fillRect(0, 0, canvas.width, canvas.height);
+  invader.update();
   player.update();
   projectiles.forEach((projectile, index) => {
     if (projectile.position.y + projectile.radius <= 0) {
