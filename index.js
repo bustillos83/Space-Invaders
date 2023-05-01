615315b845375c96f0fc836f22e22dd7713aa8b5
Fernando Bustillos <bustillos007@gmail.com>
2023-04-30 22:10:14 -0400

    ramdom invaders

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ class Projectile {
 }
 
 class Invader {
-  constructor() {
+  constructor({ position }) {
     this.velocity = {
       x: 0,
       y: 0,
@@ -99,8 +99,8 @@ class Invader {
       this.width = image.width * scale;
       this.height = image.height * scale;
       this.position = {
-        x: canvas.width / 2 - this.width / 2,
-        y: canvas.height / 2,
+        x: position.x,
+        y: position.y,
       };
     };
   }
@@ -131,9 +131,36 @@ class Invader {
     }
   }
 }
+
+class Grid {
+  constructor() {
+    this.position = {
+      x: 0,
+      y: 0,
+    };
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+    this.invaders = [];
+    const columns = Math.floor(Math.random() * 10 + 5);
+
+    const rows = Math.floor(Math.random() * 5 + 2);
+
+    for (let x = 0; x < columns; x++) {
+      for (let y = 0; y < rows; y++) {
+        this.invaders.push(new Invader({ position: { x: x * 50, y: y * 40 } }));
+      }
+    }
+    console.log(this.invaders);
+  }
+
+  update() {}
+}
 const player = new Player();
 const projectiles = [];
-const invader = new Invader();
+const grids = [new Grid()];
+
 const keys = {
   a: {
     pressed: false,
@@ -150,7 +177,7 @@ function animate() {
   requestAnimationFrame(animate);
   c.fillStyle = "black";
   c.fillRect(0, 0, canvas.width, canvas.height);
-  invader.update();
+
   player.update();
   projectiles.forEach((projectile, index) => {
     if (projectile.position.y + projectile.radius <= 0) {
@@ -161,6 +188,13 @@ function animate() {
     } else {
       projectile.update();
     }
+  });
+
+  grids.forEach((grid) => {
+    grid.update();
+    grid.invaders.forEach((invader) => {
+      invader.update();
+    });
   });
 
   //Player movement speed
