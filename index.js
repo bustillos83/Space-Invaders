9ab0738506057d9dda869d69a96d5e2ff2b60b23
Fernando Bustillos <bustillos007@gmail.com>
2023-05-04 15:55:23 -0400

    created particle

--- index.js
+++ index.js
@@ -83,6 +83,27 @@ class Projectile {
     this.position.y += this.velocity.y;
   }
 }
+class Particle {
+  constructor({ position, velocity, radius,color }) {
+    this.position = position;
+    this.velocity = velocity;
+    this.radius = radius;
+    this.color = color
+  }
+
+  draw() {
+    c.beginPath();
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+    c.fillStyle = this.color
+    c.fill();
+    c.closePath();
+  }
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
+}
 
 class InvaderProjectile {
   constructor({ position, velocity }) {
@@ -207,6 +228,7 @@ const player = new Player();
 const projectiles = [];
 const grids = [];
 const invaderProjectiles = []
+const particles = []
 
 const keys = {
   a: {
@@ -235,6 +257,9 @@ function animate() {
   c.fillRect(0, 0, canvas.width, canvas.height);
 
   player.update()
+  particles.forEach(particle =>{
+    particle.update()
+  })
   invaderProjectiles.forEach((invaderProjectile, index) =>{
 if(invaderProjectile.position.y +invaderProjectile.height >= canvas.height){
   setTimeout(() => {
